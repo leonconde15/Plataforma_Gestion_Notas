9b0587797a9490772380cc8e734594224c36d811
leonconde15 <lfconde@uninorte.edu.co>
2022-09-21 17:38:56 -0500

    Actualizado

--- app.py
+++ app.py
@@ -1,7 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app=Flask(__name__)
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('login.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+	return render_template('registro.html')
+
+@app.route('/menu')
+def menu():
+	return render_template('menu.html')
+
+@app.route('/usuarios')
+def usuarios():
+	return render_template('usuarios.html')
+
+@app.route('/materias')
+def materias():
+	return render_template('materias.html')
+
+@app.route('/cursos')
+def cursos():
+	return render_template('cursos.html')
+
+@app.route('/matriculas')
+def matriculas():
+	return render_template('matriculas.html')
+
+@app.route('/calificaciones')
+def calificaciones():
+	return render_template('calificaciones.html')
+
+@app.route('/actividades')
+def actividades():
+	return render_template('actividades.html')
+
+@app.route('/personal')
+def personal():
+	return render_template('personal.html')
+
 if __name__=='__main__':
 	app.run(debug=True)
